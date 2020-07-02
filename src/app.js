05611261ab9dc12386da947ca51eee23b7bb0723
thanabordeedev <nook.rabbit@gmail.com>
2020-07-02 12:44:04 +0700

    lab4

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+let express= require('express')
+
+const app= express()
+
+app.get('/hello/:person', function(req,res) {
+    console.log('hello -'+ req.params.person)
+    res.send('sey hello with '+ req.params.person)
+})
+
+let port = 8081
+app.listen(port, function() {
+    console.log('server running on '+ port)
+})
