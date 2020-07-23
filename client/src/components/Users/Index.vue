ee45d46c6326a6f050b8cdfb4c09c593f055dfa1
thanabordeedev <nook.rabbit@gmail.com>
2020-07-23 12:10:46 +0700

    lab7

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,11 +1,61 @@
 <template>
  <div>
     <h1>Get All Users</h1>
- </div>
+    <h4>จํานวนผู้ใช้งาน {{users.length}}</h4>
+    <p><button v-on:click="navigateTo('/user/create')">สร้างผู้ใช้งาน</button></p>
+   <div v-for="user in users" v-bind:key="user.id">
+      <p>id: {{ user.id }}</p>
+      <p>ชือ-นามสกุล: {{ user.name }} - {{ user.lastname }}</p>
+      <p>email: {{ user.email }}</p>
+      <p>password: {{ user.password }}</p>
+      <p><button v-on:click="navigateTo('/user/'+ user.id)">ดูข้อมูลผู้ใช้</button>
+      <button v-on:click="navigateTo('/user/edit/'+ user.id)">แก้ใขข้อมูล</button>
+      <button v-on:click="deleteUser(user)">ลบข้อมูล</button>
+      </p>
+
+   </div>
+   </div>
 </template>
 <script>
-export default {
+import UsersService from '@/services/UsersService'
 
+export default {
+   data () {
+      return {
+         users: []
+      }
+   },
+   async created () {
+      try{
+         this.users = (await UsersService.index()).data
+      } catch (error) {
+         console.log (error)
+      }
+   },
+   methods: {
+      navigateTo (route) {
+         this.$router.push(route)
+      },
+   },
+   methods: {
+      navigateTo (route) {
+         this.$router.push(route)
+      },
+      async deleteUser (user) {
+         let result = confirm("Want to delete?")
+         if (result) {
+            try {
+               await UsersService.delete(user)
+               this.refreshData()
+            } catch (err) {
+               console.log(err)
+            }
+         }
+      },
+      async refreshData() {
+         this.users = (await UsersService.index()).data
+      }
+   }
 }
 </script>
 <style scoped>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 
 //import
 import UserIndex from '@/components/Users/Index'
+import UserCreate from '@/components/Users/CreateUser'
+import UserEdit from '@/components/Users/EditUser'
 import UserShow from '@/components/Users/ShowUser'
 
 Vue.use(Router)
@@ -13,8 +15,19 @@ export default new Router({
       path: '/users',
       name: 'users',
       component: UserIndex
-    },{
-      path: '/user',
+    },
+    {
+      path: '/user/create',
+      name: 'users-create',
+      component: UserCreate
+    },
+    {
+      path: '/user/edit/:userId',
+      name: 'user-edit',
+      component: UserEdit
+    },
+    {
+      path: '/user/:userId',
       name: 'user',
       component: UserShow
     }
